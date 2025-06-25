e4a558341c8ed6ebd040589c3e741c1939ab5e12
BhargaviBadiginchula <bhargavibadiginchala2@gmail.com>
2025-06-25 22:48:48 +0530

    Initial commit

---
+++ firstProgram.java
@@ -0,0 +1,9 @@
+class firstProgram{
+	public static void main(String[] args){
+		System.out.println("I'm Bhargavi"):
+		System.out.println("this is my first program in java");
+	}
+}
+
+/* Every thing in java is considered as object .an object is an instance of class.
+class is an idea whereas the object makes reality of idea.

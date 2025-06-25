bda7663860aa3be8e9cefa42c1f18ca1237950b7
BhargaviBadiginchula <bhargavibadiginchala2@gmail.com>
2025-06-25 23:22:13 +0530

    first

--- firstProgram.java
+++ firstProgram.java
@@ -5,5 +5,18 @@ class firstProgram{
 	}
 }
 
-/* Every thing in java is considered as object .an object is an instance of class.
+/* Every thing in java is considered as object .An object is an instance of class.
 class is an idea whereas the object makes reality of idea.
+
+To run a Program  jvm must need signature (public static void main(String[] args))it is the entry point.
+public :access modifier -anywhere and anyone can
+static :keyword , implies method belongs to the class .Enable the method run without object.
+void :return type nothing.
+main:name of the method.
+String[] : array of strings
+args:parameter variables .values to these variable are passed from command line arguments.
+
+System : is an inbuilt class.
+out : static variable of System.
+println : is method of PrintStream.
+*/
